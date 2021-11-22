efcc1cac8c259dab544985a2f6bd10216456c207
Shane O'Hanlon <shaneaghada@gmail.com>
2021-11-22 13:47:39 +0000

    Refactor code to chain route methods

--- app.js
+++ app.js
@@ -26,40 +26,74 @@ const Article = mongoose.model('Article', articleSchema);
 
 // ----------------------------- ROUTES
 
-app.get('/articles', (req, res) => {
-    Article.find((err, foundArticles) => {
-        if (!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
-    });
-});
+// app.get('/articles', (req, res) => {
+//     Article.find((err, foundArticles) => {
+//         if (!err) {
+//             res.send(foundArticles);
+//         } else {
+//             res.send(err);
+//         }
+//     });
+// });
 
-app.post('/articles', (req, res) => {
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+app.route('/articles')
+    .get((req, res) => {
+        Article.find((err, foundArticles) => {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .post((req, res) => {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+
+        newArticle.save((err) => {
+            if (!err) {
+                res.send("Successfully added new article!");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete((req, res) => {
+        Article.deleteMany((err) => {
+            if (!err) {
+                res.send("Successfully deleted all articles.");
+            } else {
+                res.send(err);
+            }
+        });
     });
 
-    newArticle.save((err) => {
-        if (!err) {
-            res.send("Successfully added new article!");
-        } else {
-            res.send(err);
-        }
-    });
-});
+// app.post('/articles', (req, res) => {
+//     const newArticle = new Article({
+//         title: req.body.title,
+//         content: req.body.content
+//     });
 
-app.delete('/articles', (req, res) => {
-    Article.deleteMany((err) => {
-        if (!err) {
-            res.send("Successfully deleted all articles.");
-        } else {
-            res.send(err);
-        }
-    });
-});
+//     newArticle.save((err) => {
+//         if (!err) {
+//             res.send("Successfully added new article!");
+//         } else {
+//             res.send(err);
+//         }
+//     });
+// });
+
+// app.delete('/articles', (req, res) => {
+//     Article.deleteMany((err) => {
+//         if (!err) {
+//             res.send("Successfully deleted all articles.");
+//         } else {
+//             res.send(err);
+//         }
+//     });
+// });
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
