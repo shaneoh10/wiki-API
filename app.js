bb91c92f9d4b68a8fcfe9406027abdf28d7fd384
Shane O'Hanlon <shaneaghada@gmail.com>
2021-11-22 13:30:18 +0000

    Add post route to add new articles to db

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static("public"));
 
@@ -24,8 +24,33 @@ const articleSchema = {
 
 const Article = mongoose.model('Article', articleSchema);
 
-//TODO
+// ----------------------------- ROUTES
+
+app.get('/articles', (req, res) => {
+    Article.find((err, foundArticles) => {
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.post('/articles', (req, res) => {
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save((err) => {
+        if (!err) {
+            res.send("Successfully added new article!");
+        } else {
+            res.send(err);
+        }
+    });
+});
 
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
