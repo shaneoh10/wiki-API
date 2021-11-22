f040e2f339fa843289b234a160f35e83fbd42edc
Shane O'Hanlon <shaneaghada@gmail.com>
2021-11-22 13:39:33 +0000

    Add delete route method to delete all articles

--- app.js
+++ app.js
@@ -51,6 +51,16 @@ app.post('/articles', (req, res) => {
     });
 });
 
+app.delete('/articles', (req, res) => {
+    Article.deleteMany((err) => {
+        if (!err) {
+            res.send("Successfully deleted all articles.");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
